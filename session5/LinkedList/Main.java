1514904abb82b26dbcf26367faab2b47a043a139
mfreema1 <mfreema1@stevens.edu>
2019-03-14 15:41:32 -0400

    Finish LinkedList assignment

--- session5/LinkedList/Main.java
+++ session5/LinkedList/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         MyLinkedList ll = new MyLinkedList();
-        ll.loadFile("example.txt");
+        ll.loadFile("HW4b.txt");
     }    
 }
