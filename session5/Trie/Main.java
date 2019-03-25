574e73f41261651a5359cf87a0c38060b6450742
mfreema1 <mfreema1@stevens.edu>
2019-03-25 15:47:31 -0400

    Add file input

--- session5/Trie/Main.java
+++ session5/Trie/Main.java
@@ -1,14 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
+    //TODO: load in the file from Prof. Kruger and print whether
+    //or not it is in the trie
     public static void main(String[] args) {
+        String dictionaryPath = "dict.txt";
+        String inputPath = "hw5.txt";
+
         DictTrie trie = new DictTrie();
-        trie.loadFile("example.txt");
-        System.out.println(trie.isInDict("aardvark"));
-        System.out.println(trie.isInDict("actually"));
-        System.out.println(trie.isValidPrefix("carr"));
-        System.out.println(trie.isValidPrefix("foo"));
+        trie.loadFile(dictionaryPath);
+        try {
+            Scanner in = new Scanner(new File(inputPath));
+            while(in.hasNext()) {
+                System.out.println(trie.isInDict(in.next()));
+            }
+            in.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Could not load input at " + inputPath);
+        }
     }
 }
