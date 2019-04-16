6bc145e44377fecfca04b45a552da280962902fb
Mark <mfreema1@stevens.edu>
2019-04-15 21:09:05 -0400

    Continue work on matrix hw

--- session7/Matrix.java
+++ session7/Matrix.java
@@ -1,33 +1,67 @@
 public class Matrix {
 
-    private int rows;
-    private int cols;
-    private int[] m;
+    public int rows;
+    public int cols;
+    private double[] m;
 
-    public Matrix(int rows, int cols, double val) {
+    public Matrix(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
+    }
+
+    public Matrix(int rows, int cols, double val) {
+        this(rows, cols);
         for(int i = 0; i < rows * cols; i++) {
             m[i] = val;
         }
     }
 
-    public int get(int r, int c) {
+    public double get(int r, int c) {
         return m[r*cols + c];
     }
 
-    public void set(int r, int c, int val) {
+    public void set(int r, int c, double val) {
         m[r*cols + c] = val;
     }
 
     //matrix addition, can only do on same size matrices
-    public Matrix add(Matrix m) {
-
+    public static Matrix add(Matrix m1, Matrix m2) throws Exception {
+        if(m1.rows != m2.rows || m1.cols != m2.cols)
+            throw new Exception("Cannot add two different sized matrices");
+        Matrix m3 = new Matrix(m1.rows, m1.cols);
+        for(int i = 0; i < m1.rows; i++) {
+            for(int j = 0; j < m1.cols; j++) {
+                m3.set(i, j, m1.get(i, j) + m2.get(i, j));
+            }
+        }
+        return m3;
     }
 
-    //
-    public Matrix mult(Matrix m) {
+    //matrix multiplication, can only do when m1.cols = m2.rows
+    public static Matrix mult(Matrix m1, Matrix m2) throws Exception {
+        if(m1.cols != m2.rows)
+            throw new Exception("Cannot multiply, shapes do not align");
+        Matrix m3 = new Matrix(m1.rows, m2.cols);
+        for(int row = 0; row < m1.rows; row++) {
+            for(int col2 = 0; col2 < m2.cols; col2++) {
+                double sum = 0;
+                for(int row2 = 0; row2 < m2.rows; row2++) {
+                    sum += m1.get(row, col2) * m2.get(row2, col2);
+                }
+                m3.set(row, col2, sum);
+            }
+        }
+        return m3;
+    }
 
+    public static Matrix copy(Matrix m1) {
+        Matrix m2 = new Matrix(m1.rows, m1.cols);
+        for(int i = 0; i < m1.rows; i++) {
+            for(int j = 0; j < m1.cols; j++) {
+                m2.set(i, j, m1.get(i, j));
+            }
+        }
+        return m2;
     }
 
     //make a matrix object and read in from the file
