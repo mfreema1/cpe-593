c41ad272569f05cdd2c4e855479125c2edd4d8eb
Mark <mfreema1@stevens.edu>
2019-04-25 20:12:08 -0400

    Finish matrix hw

--- session7/Main.java
+++ session7/Main.java
@@ -1,6 +1,20 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        try {
+            File f = new File("hw5.dat");
+            Matrix m = new Matrix(f);
+            double[] augVector = Matrix.getAugmentationVector(f);
+            System.out.println(Arrays.toString(m.solve(augVector)));
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Could not load file");
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- session7/Matrix.java
+++ session7/Matrix.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class Matrix {
 
     public int rows;
@@ -7,13 +10,59 @@ public class Matrix {
     public Matrix(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
+        this.m = new double[rows * cols];
     }
 
     public Matrix(int rows, int cols, double val) {
         this(rows, cols);
         for(int i = 0; i < rows * cols; i++) {
-            m[i] = val;
+            this.m[i] = val;
         }
+    }
+
+    public Matrix(File f) throws FileNotFoundException {
+        Scanner in = new Scanner(f);
+        int size = in.nextInt();
+        this.rows = size;
+        this.cols = size;
+        this.m = new double[rows * cols];
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                set(i, j, in.nextInt());
+            }
+        }
+        in.close();
+    }
+
+    public static double[] getAugmentationVector(File f) throws FileNotFoundException {
+        Scanner in = new Scanner(f);
+        int size = in.nextInt();
+        for(int i = 0; i < size * size; i++, in.nextInt())
+            ; //jump over all of the numbers used in the matrix
+
+        double[] aug = new double[size];
+        for(int i = 0; i < size; i++) {
+            aug[i] = in.nextInt();
+        }
+        in.close();
+        return aug;
+    }
+
+    //not the best way to do it, I'll look into another
+    @Override
+    public String toString() {
+        StringJoiner inner = new StringJoiner("\t");
+        StringJoiner outer = new StringJoiner("\n");
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                //format to 3 decimal places, otherwise it's just ugly
+                inner.add(String.format("%.3f", (Double)get(i, j)));
+            }
+            outer.add(inner.toString());
+            inner = new StringJoiner("\t");
+        }
+        return outer.toString();
     }
 
     public double get(int r, int c) {
@@ -24,6 +73,8 @@ public class Matrix {
         m[r*cols + c] = val;
     }
 
+    //I don't think these need to be static, but the spec says they should be.  Maybe
+    //to more closely resemble an operator?
     //matrix addition, can only do on same size matrices
     public static Matrix add(Matrix m1, Matrix m2) throws Exception {
         if(m1.rows != m2.rows || m1.cols != m2.cols)
@@ -64,8 +115,81 @@ public class Matrix {
         return m2;
     }
 
-    //make a matrix object and read in from the file
-    //Ax = B
-    //x is a vector of some values
-    //full pivoting of matrix homework
+    private void swapRows(int r1, int r2) {
+        for(int i = 0; i < cols; i++) {
+            double temp = get(r2, i);
+            set(r2, i, get(r1, i));
+            set(r1, i, temp);
+        }
+    }
+
+    private void swap(double[] arr, int i1, int i2) {
+        double temp = arr[i2];
+        arr[i2] = arr[i1];
+        arr[i1] = temp;
+    }
+
+    private int getFirstRowWithNonzero(int rowsCompleted) throws Exception {
+        for(int i = rowsCompleted; i < cols; i++) {
+            for(int j = rowsCompleted; j < rows; j++) {
+                if(get(j, i) != 0)
+                    return j;
+            }
+        }
+        throw new Exception("Solution could not be found");
+    }
+
+    private double getFirstNonzeroTermOnRow(int row) throws Exception {
+        for(int i = 0; i < cols; i++) {
+            double term = get(row, i);
+            if(term != 0)
+                return term;
+        }
+        throw new Exception("Row found to have only zeroes");
+    }
+
+    //solve matrix m with the augmentation matrix aug
+    public double[] solve(double[] aug) throws Exception{
+        if(rows != aug.length)
+            throw new Exception("Incorrect lengths for matrix and augmentation vector");
+        for(int i = 0; i < rows; i++) {
+
+            //bring the row with the first non-zero term to the top of workable rows 
+            int nextRow = getFirstRowWithNonzero(i);
+            if(nextRow != i) {
+                swapRows(nextRow, i);
+                swap(aug, nextRow, i);
+            }
+
+            swapRows(getFirstRowWithNonzero(i), i); 
+            double leadingTerm = getFirstNonzeroTermOnRow(i);
+
+            //now divide the row and augmentation vector by the leading term
+            for(int j = i; j < cols; j++) {
+                set(i, j, get(i, j) / leadingTerm);
+            }
+            aug[i] = aug[i] / leadingTerm;
+
+            //now add multiples of the previous row to zero out terms below the 1 we just created
+            for(int j = i + 1; j < rows; j++) {
+                double multiplier = get(j, i) * -1; //the term (i,i) is always 1 here
+                for(int k = i; k < cols; k++) {
+                    set(j, k, get(j, k) + get(i, k) * multiplier);
+                }
+                aug[j] += aug[i] * multiplier;
+            }
+        }
+
+        //now we should have an upper-right triangular matrix and an augmentation vector that we can use to generate
+        //our solution.  Walk the matrix (avoiding the diagonal) going upward and generate our solution from the bottom up.
+        for(int i = rows - 2; i >= 0; i--) {
+            double sum = 0;
+            for(int j = i + 1; j < cols; j++) {
+                sum += get(i, j) * aug[j];
+            }
+            aug[i] -= sum;
+        }
+
+        return aug;
+    }
 }
